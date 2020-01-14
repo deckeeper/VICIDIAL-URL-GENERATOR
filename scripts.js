5534777acfb538f4aaebddb7eb146c4e444d03ff
Bill Botonis <billbotonis@gmail.com>
2020-01-14 11:10:53 -0500

    updated javascript file

--- scripts.js
+++ scripts.js
@@ -3,22 +3,22 @@ document.getElementById("create").addEventListener("click", function(e){
  e.preventDefault(); 
   const seperator = document.getElementById('seperator').value;
  if(seperator=='0'){
-  	var values = document.getElementById("link_values").value.split("\n");
+  	let values = document.getElementById("link_values").value.split("\n");
   }
   else{
-  	var values = document.getElementById("link_values").value.split(seperator);
+  	let values = document.getElementById("link_values").value.split(seperator);
   }
-  var text = '';
+  let text = '';
  
 
-for (var i =0; i < values.length; i++) {
+for (let i =0; i < values.length; i++) {
 	if (i==0){
-		var path = document.getElementById('script_path').value+ '?' + values[i] + '=--A--' + values[i] + '--B--';
+		let path = document.getElementById('script_path').value+ '?' + values[i] + '=--A--' + values[i] + '--B--';
 		text = text + path;
 	}
 
 	else{
-		var option = '&' + values[i] + '=--A--' + values[i] + '--B--' ;
+		let option = '&' + values[i] + '=--A--' + values[i] + '--B--' ;
 		text = text + option;
 	}
 	
